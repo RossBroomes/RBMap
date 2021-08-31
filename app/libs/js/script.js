b2db63e304e5ddd54ab33cb37056bdb7bc4cea20
Ross <RossBroomes@Gmail.com>
2021-08-31 12:30:08 +0100

    Conversion

--- app/libs/js/script.js
+++ app/libs/js/script.js
@@ -41,24 +41,18 @@ $('#selCountry').change(function() {
 						$('#speed').text(response.wind.speed);
 						
 						$('#country').text(response.sys.country);
-						$('#sunset').text(response.sys.sunset);
+						$('#sunset').text(response.sys.sunset)
+						var date = new Date("#sunset" * 1000)
+						hours = date.getHours();
+						minutes = "0" + date.getMinutes();
+						seconds = "0" + date.getSeconds();
+						formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+						console.log(formattedTime);
+						;
+						
 						$('#sunrise').text(response.sys.sunrise);
 
-						let unix_timestamp = ['sunset']
-						// Create a new JavaScript Date object based on the timestamp
-						// multiplied by 1000 so that the argument is in milliseconds, not seconds.
-						var date = new Date(unix_timestamp * 1000);
-						// Hours part from the timestamp
-						var hours = date.getHours();
-						// Minutes part from the timestamp
-						var minutes = "0" + date.getMinutes();
-						// Seconds part from the timestamp
-						var seconds = "0" + date.getSeconds();
-
-						// Will display time in 10:30:23 format
-						var formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
-
-						console.log(formattedTime);
+						
 
 						// let main = response.main.temp;
 						// $('#temp').text(main);
@@ -95,4 +89,18 @@ $('#selCountry').change(function() {
 	
 	});
 
-    
+						
+						// // Create a new JavaScript Date object based on the timestamp
+						// // multiplied by 1000 so that the argument is in milliseconds, not seconds.
+						// var date = new Date("sunset" * 1000);
+						// // Hours part from the timestamp
+						// var hours = date.getHours();
+						// // Minutes part from the timestamp
+						// var minutes = "0" + date.getMinutes();
+						// // Seconds part from the timestamp
+						// var seconds = "0" + date.getSeconds();
+
+						// // Will display time in 10:30:23 format
+						// var formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+
+						// console.log(formattedTime);
